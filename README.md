a6282d55c7028e4861ccc8bb4645fa8004508e47
Espi <joshespi@gmail.com>
2023-02-10 09:38:23 -0700

    initial upload of version 1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pcsd-collapse
 simple wordpress plugin to add collapse function for our theme
+
+
+
+## Instructions.
+
+control (click) element needs to have the class .collapseContol
+the collapsing content needs to have the class .collapseContent
+
+
+## Example:
+```
+<p class="collapsible">Open Collapsible</p>
+<div class="colcontent">
+  <p>Lorem ipsum...</p>
+</div>
+```
---
+++ pcsd-collapse/pcsd-collapse.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Plugin Name: PCSD Collapse
+* Plugin URI: https://github.com/Provo-City-School-District/pcsd-collapse
+* Description: add a collapsible feature
+* Author: Josh Espinoza
+* Version: 1.0
+* License: GPLv2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ 
+instructions use: https://www.w3schools.com/howto/howto_js_collapsible.asp
+
+Instructions.
+
+control (click) element needs to have the class .collapseContol
+the collapsing content needs to have the class .collapseContent
+
+
+example:
+
+<p class="collapsible">Open Collapsible</p>
+<div class="colcontent">
+  <p>Lorem ipsum...</p>
+</div>
+
+*/
+function pcsd_collapse_assets()
+{
+    wp_register_script('collapse_js', plugin_dir_url(__FILE__) . '/pcsd-collapse.js', array(), '1.0', true);
+    wp_enqueue_script('collapse_js');
+    wp_enqueue_style('collapse_css', plugin_dir_url(__FILE__) . '/pcsd-collapse.css', '', '1.0', false);
+}
+add_action('wp_enqueue_scripts', 'pcsd_collapse_assets', 999);
